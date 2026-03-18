d8f6a9dbf00767f93ea32f75221264488273606d
Shmuel Brandt <brandt.shmuel@gmail.com>
2026-03-18 23:22:41 +0200

    imporoved animation on hero page and added people page alongside quotes station

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const sitePages = [
   { title: "דף הבית", path: "/" },
   { title: "המסלול", path: "/track" },
+  { title: "האנשים", path: "/people" },
   { title: "המיונים", path: "/selection" },
   { title: "אתגרים", path: "/challenges" },
   { title: "שאלות ותשובות", path: "/faq" },
