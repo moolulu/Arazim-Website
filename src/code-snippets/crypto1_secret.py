cacf6d4d596fc03c5a782d2bd1d06f520fe3da27
moolulu <brandt.shmuel@gmail.com>
2026-03-29 17:46:28 +0300

    improved progress bar

--- src/code-snippets/crypto1_secret.py
+++
@@ -1,5 +0,0 @@
-from Crypto.Util.number import getPrime
-
-bitsize = 512
-key = b"She sells sea-shells by the sea-shore"
-p,q = getPrime(bitsize), getPrime(bitsize)
--- src/code-snippets/crypto1_solve.py
+++
@@ -1,8 +0,0 @@
-from challenge import Challenge
-
-def main():
-    chal = Challenge()
-    print(chal.encrypt())
-
-if __name__ == "__main__":
-    main()
