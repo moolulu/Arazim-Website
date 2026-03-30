d8b21dd2523bbd6c43ec94ef7df12b3680477eee
moolulu <brandt.shmuel@gmail.com>
2026-03-30 20:02:49 +0300

    reduced lag

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://moolulu.github.io',
   base: 'Arazim-Website',
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'hover'
+  }
 });
