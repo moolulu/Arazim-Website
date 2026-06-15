9e0991616b0163aa2fcc6c5cf5b16c665d87b385
moolulu <brandt.shmuel@gmail.com>
2026-06-15 15:55:44 +0300

    generate sitemaps automatically

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://moolulu.github.io',
   base: 'Arazim-Website',
+
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'hover'
-  }
+  },
+
+  integrations: [sitemap()]
 });
